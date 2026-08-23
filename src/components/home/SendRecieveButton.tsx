7221272ed2e9917fef0613600250a44d51a239f5
MOHSIN-JAWADWALA <mohsinjawadwala8@gmail.com>
2026-08-23 18:23:29 +0530

    Eliminate errors

---
+++ src/components/home/SendRecieveButton.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SendRecieveButton = () => {
+  return (
+    <View>
+      <Text>SendRecieveButton</Text>
+    </View>
+  )
+}
+
+export default SendRecieveButton
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React, { FC } from 'react'
 import { commonStyles } from '../styles/commonStyles'
 import HomeHeader from '../components/home/HomeHeader'
+import SendRecieveButton from '../components/home/SendRecieveButton'
+import Options from '../components/home/Options'
+import Misc from '../components/home/Misc'
 
 const HomeScreen:FC = () => {
   return (
     <View style={commonStyles.baseContainer}>
       <HomeHeader />
+      <ScrollView
+      contentContainerStyle={{paddingBottom:100,padding:15}}
+      showsVerticalScrollIndicator={false}
+      >
+        <SendRecieveButton />
+        <Options />
+        <Misc />
+      </ScrollView>
     </View>
   )
 }
