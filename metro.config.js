59a84db2fe9af914597b77d783290f3a30d19294
MOHSIN-JAWADWALA <mohsinjawadwala8@gmail.com>
2026-08-23 18:18:42 +0530

    Add remaining files

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,30 @@
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
+  },
+  resolver: {
+    assetExts: [...assetExts.filter((ext) => ext !== 'svg'), 'pem', 'p12'],
+    sourceExts: [...sourceExts, 'svg'],
+    extraNodeModules: {
+      'react-native/asset-registry': path.resolve(
+        __dirname,
+        'node_modules/react-native/Libraries/Image/AssetRegistry'
+      ),
+    },
+  },
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, config);
